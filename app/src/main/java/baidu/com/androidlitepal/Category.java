bd6c0b166f072884c769556edb394aa365d4352c
Janieck <1978684748@qq.com>
2017-10-11 22:37:31 +0800

    Add Database

---
+++ app/src/main/java/baidu/com/androidlitepal/Category.java
@@ -0,0 +1,36 @@
+package baidu.com.androidlitepal;
+
+/*
+ *  创建者:   Administrator
+ *  创建时间:  2017/10/11 21:14
+ *  描述：    TODO
+ */
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
--- app/src/main/java/baidu/com/androidlitepal/MainActivity.java
+++ app/src/main/java/baidu/com/androidlitepal/MainActivity.java
@@ -1,13 +1,40 @@
 package baidu.com.androidlitepal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mCreateDatabase;
+    private Button mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCreateDatabase = (Button) findViewById(R.id.create_database);
+        mCreateDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+            }
+        });
+        mAddButton = (Button) findViewById(R.id.add_database);
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("The Da Vinci Code");
+                book.setAuthor("Dan Brown");
+                book.setPages(454);
+                book.setPrice(16.69);
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
     }
 }
